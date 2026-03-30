49d553e2c34304874179af06a80d62ac1d997fb4
akshat-11004 <akshatkadia02@ggmail.com>
2026-03-30 21:48:18 +0530

    Implement streamlit library in app.py

--- app.py
+++ app.py
@@ -1,19 +1,32 @@
 import streamlit as st
-import numpy as np
+from sklearn.datasets import load_iris
+from sklearn.ensemble import RandomForestClassifier
 import pandas as pd
 
-st.title("Hello")
+@st.cache_data
 
-st.write("This is Simple text")
+def load_data():
+    iris = load_iris()
+    df = pd.DataFrame(iris.data,columns=iris.feature_names)
+    df['species'] = iris.target
+    return df,iris.target_names
 
-df = pd.DataFrame({
-    'first column':[1,2,3,4],
-    'second column' : [5,6,7,8]
-})
+df,target_name = load_data()
 
-st.write(df)
+model = RandomForestClassifier()
+model.fit(df.iloc[:,:-1],df['species'])
 
-chart_data = pd.DataFrame(
-    np.random.rand(20,3),columns=['a','b','c']
-)
-st.line_chart(chart_data)
+st.sidebar.title("Input Features")
+sepal_length = st.sidebar.slider("sepal length",float(df['sepal length (cm)'].min()),float(df['sepal length (cm)'].max()))
+sepal_width = st.sidebar.slider("sepal width",float(df['sepal width (cm)'].min()),float(df['sepal width (cm)'].max()))
+petal_length = st.sidebar.slider("petal length",float(df['petal length (cm)'].min()),float(df['petal length (cm)'].max()))
+petal_width = st.sidebar.slider("petal width",float(df['petal width (cm)'].min()),float(df['petal width (cm)'].max()))
+
+input_data = [[sepal_length,sepal_width,petal_length,petal_width]]
+
+prediction = model.predict(input_data)
+predicted_species = target_name[prediction[0]]
+
+
+st.write("prediction")
+st.write(f"Predicted species is {predicted_species}")
